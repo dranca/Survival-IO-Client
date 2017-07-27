3f8f7eda7ed123b18566e9fffc6d0af674463d69
Alexandru Dranca <dranca.af@gmail.com>
2017-07-28 00:20:46 +0300

    Adds the second scene and unit testing posibility.

---
+++ Assets/Scripts/ConnectionMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionMenuUI : MonoBehaviour {
+    SFS2XConnectInput networking;
+    // Use this for initialization
+
+    public InputField textField;
+
+	void Start () {
+        findNetworking();
+
+        if (networking == null)
+        {
+            Debug.LogError("The networking component was not found. Please make sure to fix this");
+        }
+	}
+    
+    private void findNetworking()
+    {
+        networking = GameObject.Find("Networking").GetComponent<SFS2XConnectInput>();
+    }
+
+    public void UserSelectedConnect()
+    {
+        string userInput = textField.text;
+        if (networking != null)
+        {
+            networking.signInWithName(userInput);
+        } else {
+            Debug.LogError("Networking Not found.");
+            findNetworking();
+        }
+    }
+
+}
